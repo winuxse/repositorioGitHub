ecdb639cefa6b824d1e5586efe265fd256698664
Daniel <winux.se@gmail.com>
2020-11-15 19:25:41 -0300

    primerCommit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Opcional.
+const options = {
+    method: "GET",
+    headers: {
+        // Para deshabilitar CORS:
+        //'Access-Control-Allow-Origin' : 'http://127.0.0.1:5500'
+        'Access-Control-Allow-Origin' : '*'
+        //'Access-Control-Allow-Origin' : 'http://127.0.0.1:5500/index.html'
+    }
+};
